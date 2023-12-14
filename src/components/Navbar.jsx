274a61cfa1887470234ec7f59fd21b99d231b09f
gaurav3278 <gauravrai54152@gmail.com>
2023-12-14 11:49:02 +0530

    changes by gaurav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,19 +6,16 @@ import { useNavigate } from 'react-router';
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <div className='bg-[#F9FAFB] justify-around flex py-4 px-20 shadow-sm'>
-      <div className='container flex items-start'>
+    <div className='bg-[#F9FAFB] flex justify-between py-4 px-20 shadow-sm'>
+      <div className='container w-max flex'>
         <img src={logo} alt="logo" style={{ width: '44px', height: '44px' }} />
       </div>
-      <div className='w-full flex items-center justify-evenly'>
+      <div className='flex items-center gap-5 '>
         <div className='flex gap-1 items-center cursor-pointer'>
           <h1 className='text-[#363F54] font-bold'>Find a mentor</h1>
           <ArrowDropDownIcon sx={{ color: '#00D1B2' }} />
         </div>
-        <div className='flex gap-1 items-center cursor-pointer'>
-          <h1 className='text-[#363F54] font-bold'>For Businesses</h1>
-          <ArrowDropDownIcon sx={{ color: '#00D1B2' }} />
-        </div>
+        <div className='flex gap-1 items-center cursor-pointer font-bold'>Become a mentor</div>
         <h1 className='text-[#363F54] font-bold cursor-pointer'>Book a session</h1>
         <div className='bg-[#DEF7EC] p-2 rounded-md cursor-pointer'>
           <span className='text-[#057A55] font-semibold font-sans' onClick={() => { navigate("/signup") }}>Getting Started</span>
