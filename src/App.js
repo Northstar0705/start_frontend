8d65bd31a8d66f77470d6b73a1926f54de42764c
ayushsingh0607 <ayushraj0607@gmail.com>
2023-12-15 18:03:40 +0530

    ayush commits recent changes.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Landing from './pages/Landing';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup.jsx';
 import Login from './pages/Login.jsx';
+import Mentor from './pages/Mentor/Mentor.jsx';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Landing />} />
+        <Route exact path='/' element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/mentor" element={<Mentor />} />
       </Routes>
     </div>
   );
